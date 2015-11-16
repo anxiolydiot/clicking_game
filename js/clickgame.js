cf6d8f0879b42a66a2e77da87f6c470ef9e20baf
Sean Conaty <anxiolydiot@Seans-MacBook-Pro.local>
2015-11-16 00:08:33 -0500

    working out random array and getting it into html

--- js/clickgame.js
+++ js/clickgame.js
@@ -1,8 +1,25 @@
-// alertEnd=tells user times up. 
-
-var srcList = $('img').map(function() {
+/*var srcList = $('img').map(function() {
     return this.src;
-}).get();
+}).get(); */ 
+
+var randomArrayOut= function shuff (imgArray){
+	var currentIndex = imgArray.length, temporayValue, randomIndex;
+	while (0 !== currentIndex) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+    	imgArray[currentIndex] = imgArray[randomIndex];
+    	imgArray[randomIndex] = temporaryValue;
+
+	}
+	return imgArray;
+}
+
+
+var = index; 
+for (index = 0; index < randomArrayOut.length; ++index) {
+    console.log(randomArrayOut[index]);
+}
 
 
 
